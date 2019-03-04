06bc3ab62b556c7284fbf0734be29658b37bb1b7
ansara.hooks <ansara.hooks@gmail.com>
2019-03-04 18:50:53 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,14 @@
-
-
-def reverse_each_word(sentence)
-  sentence.split.collect {|word| word.reverse}.join(" ")
+def reverse_each_word (string)
+  new_array = string.split(" ")
+  new_sentence = []
+  new_array.collect do |word|
+    end_loop = word.length
+    new_string = ""
+    while end_loop > 0
+      end_loop -= 1
+      new_string += word[end_loop]
+    end
+    new_sentence << new_string
+  end
+  new_sentence.join(" ")
 end
